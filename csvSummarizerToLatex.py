0cc126dc1e6b0ec5db27b28480e4f32f9586b897
AbeKenshi <abeken1224@yahoo.co.jp>
2016-11-26 05:41:48 +0900

    エラーチェック追加

--- csvSummarizerToLatex.py
+++ csvSummarizerToLatex.py
@@ -3,28 +3,34 @@ import pandas as pd
 import numpy as np
 
 
+def summarizeCsvToLatex(name, significant_figure, multipliers):
+    df = pd.DataFrame.from_csv(name + ".csv")  # ファイル読み込み
+    if len(df.columns) != len(significant_figure):
+        raise ValueError("The length of the array of significant figure does not match the number of columns of data.")
+    if len(df.columns) != len(multipliers):
+        raise ValueError("The length of the array of multiplier does not match the number of columns of data.")
+    for i in range(len(df.columns)):
+        if multipliers[i] != None:
+            df.ix[:, [i]] = df.ix[:, [i]] / (10 ** multipliers[i])
+        if significant_figure[i] != None:
+            s_orig = df.ix[:, [i]].as_matrix()
+            s_orig = round_sig(s_orig, significant_figure[i])
+            dfn = pd.DataFrame(s_orig)
+            dfn.columns = [df.columns[i]]
+            df.ix[:, [i]] = dfn
+
+    df.to_csv(name + ".tex", sep='&', line_terminator='\\\\ \hline\n', quotechar=" ")
+
+
+# 有効桁数に丸め込むメソッド
 def round_sig(x, sig=2):
-    y = [np.around(x[i], sig - np.int_(np.floor(np.log10(abs(x[i] + 1e-200)))) - 1) for i in range(len(x))]
+    y = [np.around(x[i], sig - np.int_(np.floor(np.log10(abs(x[i] + 1e-200)))) - 1) if
+         not (np.math.isnan(x[i])) else x[i] for i in range(len(x))]
     return y
 
 
-significant_figure = [None, 3, 3]  # 有効桁数，Noneの場合は桁数調整をしない
-multipliers = [None, 4, 4]  # 各列のデータを10の乗数で割る，Noneの場合は乗算をしない
-
-name = "sample/sample"  # ファイル名
-df = pd.DataFrame.from_csv(name + ".csv")  # ファイル読み込み
-df.columns = ["成功回数",
-              "平均評価回数($\\times10^" + str(multipliers[1]) + "$)",
-              "標準偏差($\\times10^" + str(multipliers[2]) + "$)"]
-for i in range(len(df.columns)):
-    if multipliers[i] != None:
-        df.ix[:, [i]] = df.ix[:, [i]] / (10 ** multipliers[i])
-    if significant_figure[i] != None:
-        s_orig = df.ix[:, [i]].as_matrix()
-        s_orig = round_sig(s_orig, significant_figure[i])
-        # print(pd.DataFrame(s_orig))
-        dfn = pd.DataFrame(s_orig)
-        dfn.columns = [df.columns[i]]
-        df.ix[:, [i]] = dfn
-
-df.to_csv(name + ".tex", sep='&', line_terminator='\\\\ \hline\n', quotechar=" ")
+if __name__ == '__main__':
+    name = "sample/sample"
+    significant_figure = [None, 3, 3]  # 有効桁数，Noneの場合は桁数調整をしない
+    multipliers = [None, 4, 4]  # 各列のデータを10の乗数で割る，Noneの場合は乗算をしない
+    summarizeCsvToLatex(name, significant_figure, multipliers)
